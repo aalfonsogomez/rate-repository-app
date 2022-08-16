443a3238f9657387c061e7aa04134dc09e416168
Angel <a.alfonso.gomez@gmail.com>
2022-08-16 11:37:32 +0200

    Dejamos app.js libre y llevamos el inicio a main.js

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import Constants from 'expo-constants';
+
+const Main = () => {
+    return (
+        <View style={{ marginTop: Constants.statusBarHeight, flexGrow: 1}}>
+            <Text>Rate Repository Application</Text>
+        </View>
+    );
+};
+
+export default Main;
