7f1e1003fe59110c7e74660def0b0f14da76b5ac
Angel <a.alfonso.gomez@gmail.com>
2022-08-17 12:22:58 +0200

    Estilamos con StyleSheet de react-native

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,8 +1,8 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const RepositoryItem = (props) => (
-    <View key={props.id} style={{ padding: 20, paddingBottom: 5, paddingTop: 5 }}>
-        <Text style={{ fontWeight: 'bold', marginBottom: 5 }}>{props.id}</Text>
+    <View key={props.id} style={styles.container}>
+        <Text style={styles.strong}>{props.id}</Text>
         <Text>{props.fullName}</Text>
         <Text>{props.description}</Text>
         <Text>{props.language}</Text>
@@ -12,5 +12,18 @@ const RepositoryItem = (props) => (
         <Text>{props.ratingAverage}</Text>
     </View>
 )
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        paddingBottom: 5,
+        paddingTop: 5
+    },
+    strong: {
+        color: '#09f',
+        fontWeight: 'bold',
+        marginBottom: 5
+    }
+})
 
 export default RepositoryItem;
