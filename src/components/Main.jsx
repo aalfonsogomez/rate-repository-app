8191bb69ba161f989354c03558c9810afb12596d
Angel <a.alfonso.gomez@gmail.com>
2022-08-18 11:47:21 +0200

    Añadimos nueva dependencia react-router-native y ajustamos el Main.jsx con rutas

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,16 @@
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
+import { Routes, Route } from 'react-router-native';
 
 const Main = () => {
     return (
-        <View>
+        <View style={{ flex: 1}}>
             <AppBar />
-            <RepositoryList />
+            <Routes>
+                <Route path='/' exact element={<RepositoryList />} />
+                <Route path='/sigin' exact element={<Text>Working on it</Text>} />
+            </Routes>
         </View>
     );
 };
