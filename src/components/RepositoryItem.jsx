6b502c4fd21acd28d6479db767cf8329f8bfe9e2
Angel <a.alfonso.gomez@gmail.com>
2022-08-18 10:31:05 +0200

    Creamos el componente RepositoryItemHeader y lo estilamos

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,13 +1,10 @@
-import { View, StyleSheet } from 'react-native';
-import StyledText from './StyledText';
+import { View, StyleSheet, Image } from 'react-native';
 import RepositoryStats from './RepositoryStats';
-import theme from '../theme';
+import RepositoryItemHeader from './RepositoryItemHeader';
 
 const RepositoryItem = (props) => (
     <View key={props.id} style={styles.container}>
-        <StyledText fontSize='subheading' fontWeight='bold'>{props.fullName}</StyledText>
-        <StyledText>{props.description}</StyledText>
-        <StyledText style={styles.language}>{props.language}</StyledText>
+        <RepositoryItemHeader {...props} />
         <RepositoryStats {...props} />
     </View>
 )
@@ -15,17 +12,7 @@ const RepositoryItem = (props) => (
 const styles = StyleSheet.create({
     container: {
         padding: 20,
-        paddingBottom: 5,
-        paddingTop: 5
-    },
-    language: {
-        padding: 4,
-        color: theme.colors.white,
-        backgroundColor: theme.colors.primary,
-        alignSelf: 'flex-start',
-        borderRadius: 4,
-        overflow: 'hidden'
-
+        paddingVertical: 5
     }
 })
 
---
+++ src/components/RepositoryItemHeader.jsx
@@ -0,0 +1,35 @@
+import { View, StyleSheet, Image } from 'react-native';
+import StyledText from './StyledText';
+import theme from '../theme';
+
+const RepositoryItemHeader = ({ownerAvatarUrl, fullName, description, language}) => (
+    <View style={{ flexDirection: 'row', paddindBottom: 2}}>
+        <View style={{paddingRight: 10}}>
+            <Image style={styles.image} source={{ uri: ownerAvatarUrl }} />
+        </View>
+        <View style={{}}>
+            <StyledText fontWeight='bold'>{fullName}</StyledText>
+            <StyledText color='secondary'>{description}</StyledText>
+            <StyledText style={styles.language}>{language}</StyledText>
+        </View>
+    </View>
+);
+
+const styles = StyleSheet.create({
+    language: {
+        padding: 4,
+        color: theme.colors.white,
+        backgroundColor: theme.colors.primary,
+        alignSelf: 'flex-start',
+        marginVertical: 4,
+        borderRadius: 4,
+        overflow: 'hidden'
+    },
+    image: {
+        width: 48,
+        height: 48,
+        borderRadius: 4
+    }
+})
+
+export default RepositoryItemHeader;
